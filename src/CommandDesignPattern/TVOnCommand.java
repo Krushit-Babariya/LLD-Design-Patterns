1997d30be594059777d09a9effd066e890a41cd7
Krushit-Babariya <krushitsbabariya41@gmail.com>
2025-12-28 15:11:59 +0530

    Observer & Command Pattern

---
+++ src/CommandDesignPattern/TVOnCommand.java
@@ -0,0 +1,24 @@
+package CommandDesignPattern;
+
+public class TVOnCommand implements Command {
+    private final TV tv;
+    private boolean previousState;
+
+    public TVOnCommand(TV tv) {
+        this.tv = tv;
+    }
+
+    @Override
+    public void execute() {
+        previousState = tv.isOn();
+        tv.turnOn();
+    }
+
+    @Override
+    public void undo() {
+        if (!previousState) {
+            tv.turnOff();
+        }
+    }
+}
+
